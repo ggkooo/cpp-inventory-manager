114e7f5b54996b50138215860099f7e3e606697a
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-08-25 13:50:36 -0300

    feat: added data persistence method (product)

--- src/models/product.h
+++ src/models/product.h
@@ -1,16 +1,25 @@
 #pragma once
 #include <string>
+#include <memory>
+#include <vector>
+#include "db_connection.h"
 
 class Product {
 public:
     Product(int id, const std::string& name, int categoryId, int quantity);
 
-    int getId() const;
-    std::string getName() const;
-    int getCategoryId() const;
-    int getQuantity() const;
+    [[nodiscard]] int getId() const;
+    [[nodiscard]] std::string getName() const;
+    [[nodiscard]] int getCategoryId() const;
+    [[nodiscard]] int getQuantity() const;
 
     void setQuantity(int quantity);
+
+    static void insert(const std::shared_ptr<sql::Connection>& conn, const std::string& name, int categoryId, int quantity);
+    static std::vector<Product> getAll(const std::shared_ptr<sql::Connection>& conn);
+    static Product getById(const std::shared_ptr<sql::Connection>& conn, int id);
+    static void update(const std::shared_ptr<sql::Connection>& conn, int id, const std::string& name, int categoryId, int quantity);
+    static void remove(const std::shared_ptr<sql::Connection>& conn, int id);
 private:
     int id;
     std::string name;
